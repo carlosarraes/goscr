ac2d4c16df34bd15128805c87416ea5d782f550b
carraes <carraeshb@gmail.com>
2023-03-09 16:07:25 -0300

    feat: writes json file

--- scapper.go
+++ scapper.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
-type item struct {
+type scrapper struct {
 	Img            string `json:"img"`
 	Character      string `json:"character"`
 	DevilFruit     string `json:"devilFruit"`
@@ -18,11 +20,29 @@ func main() {
 		colly.AllowedDomains("listfist.com"),
 	)
 
-	c.OnHTML("td", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+	var Data []scrapper
+
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		item := scrapper{
+			Img:            e.ChildAttr("img", "src"),
+			Character:      e.ChildText("td.col-2"),
+			DevilFruit:     e.ChildText("td.col-3"),
+			DevilFruitType: e.ChildText("td.col-4"),
+		}
+
+		Data = append(Data, item)
 	})
 
 	if err := c.Visit("https://listfist.com/list-of-one-piece-devil-fruits"); err != nil {
+		fmt.Println(err)
+	}
+
+	content, err := json.Marshal(Data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if err = os.WriteFile("data.json", content, 0644); err != nil {
 		fmt.Println(err)
 	}
 }
